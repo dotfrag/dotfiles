ddcaaac224494b95c988dec8165f96e1d0956c1c
dotfrag <17456867+dotfrag@users.noreply.github.com>
2022-09-12 00:29:21 +0300

    .

--- config/polybar/scripts/pulseaudio-microphone.sh
+++ config/polybar/scripts/pulseaudio-microphone.sh
@@ -1,63 +1,63 @@
 #!/bin/bash
 
 status() {
-    DEFAULT_SOURCE_INDEX=$(pacmd list-sources | grep "\* index:" | cut -d' ' -f5)
-    volume=$(pacmd list-sources | grep "index: $DEFAULT_SOURCE_INDEX" -A 7 | grep "volume" | awk -F/ '{print $2}')
-    muted=$(pacmd list-sources | grep "index: $DEFAULT_SOURCE_INDEX" -A 11 | grep "muted")
+  DEFAULT_SOURCE=$(pactl get-default-source)
+  volume=$(pactl list sources | grep "$DEFAULT_SOURCE" -m1 -A7 | grep "Volume" | awk -F'/' '{print $2}')
+  mute=$(pactl list sources | grep "$DEFAULT_SOURCE" -m1 -A7 | grep "Mute")
 
-    if [ -z "$volume" ]; then
-        echo "No Mic Found"
+  if [ -z "$volume" ]; then
+    echo "No Mic Found"
+  else
+    volume="${volume//[[:blank:]]/}"
+    if [[ "$mute" == *"yes"* ]]; then
+      echo " MUTED"
+    elif [[ "$mute" == *"no"* ]]; then
+      echo "%{F#EBCB8B} OPEN%{F-} $volume"
     else
-        volume="${volume//[[:blank:]]/}"
-        if [[ "$muted" == *"yes"* ]]; then
-            echo " MUTED"
-        elif [[ "$muted" == *"no"* ]]; then
-            echo "%{F#EBCB8B} OPEN%{F-} $volume"
-        else
-            echo "%{F#EBCB8B} OPEN%{F-} $volume !"
-        fi
+      echo "%{F#EBCB8B} OPEN%{F-} $volume !"
     fi
+  fi
 }
 
 listen() {
-    status
+  status
 
-    LANG=EN
-    pactl subscribe | while read -r event; do
-        if echo "$event" | grep -q "source" || echo "$event" | grep -q "server"; then
-            status
-        fi
-    done
+  LANG=EN
+  pactl subscribe | while read -r event; do
+    if echo "$event" | grep -q "source" || echo "$event" | grep -q "server"; then
+      status
+    fi
+  done
 }
 
 toggle() {
-    pactl set-source-mute @DEFAULT_SOURCE@ toggle
+  pactl set-source-mute @DEFAULT_SOURCE@ toggle
 }
 
 increase() {
-    DEFAULT_SOURCE_INDEX=$(pacmd list-sources | grep "\* index:" | cut -d' ' -f5)
-    volume=$(pacmd list-sources | grep "index: $DEFAULT_SOURCE_INDEX" -A 7 | grep "volume" | awk -F/ '{print $2}')
+  DEFAULT_SOURCE=$(pactl get-default-source)
+  volume=$(pactl list sources | grep "$DEFAULT_SOURCE" -m1 -A7 | grep "Volume" | awk -F'/' '{print $2}')
 
-    if (( ${volume//%} < 100 )); then
-      pactl set-source-volume @DEFAULT_SOURCE@ +5%
-    fi
+  if ((${volume//%/} < 100)); then
+    pactl set-source-volume @DEFAULT_SOURCE@ +5%
+  fi
 }
 
 decrease() {
-    pactl set-source-volume @DEFAULT_SOURCE@ -5%
+  pactl set-source-volume @DEFAULT_SOURCE@ -5%
 }
 
 case "$1" in
 --toggle)
-    toggle
-    ;;
+  toggle
+  ;;
 --increase)
-    increase
-    ;;
+  increase
+  ;;
 --decrease)
-    decrease
-    ;;
+  decrease
+  ;;
 *)
-    listen
-    ;;
+  listen
+  ;;
 esac

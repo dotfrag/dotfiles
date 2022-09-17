0d5d28ba1c514ceeaa1b70869b941131fb6d72ab
dotfrag <17456867+dotfrag@users.noreply.github.com>
2022-09-17 11:53:15 +0300

    .

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -7,13 +7,21 @@ killall -q -9 polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+# Variables
+MONITORS=$(polybar --list-monitors | cut -d":" -f1)
+PRIMARY_MONITOR=$(polybar --list-monitors | grep "primary" | cut -d":" -f1)
+if [ -z "${POLYBAR_INTF_TYPE}" ]; then
+  has_ethernet=$(nmcli device | awk '$2=="ethernet" {print $1}' | head -1 | wc -l)
+  if [ "${has_ethernet}" -eq 0 ]; then
+    export POLYBAR_INTF_TYPE="wireless"
+  fi
+fi
+
 # Launch the bar
 printf "\n-------------------\n" | tee -a /tmp/polybar_top.log /tmp/polybar_bottom.log
-monitors=$(polybar --list-monitors | cut -d":" -f1)
-primary_monitor=$(polybar --list-monitors | grep "primary" | cut -d":" -f1)
-for m in ${monitors[@]}; do
+for m in "${MONITORS[@]}"; do
   export MONITOR=$m
-  if [ "$m" = "$primary_monitor" ]; then
+  if [ "${m}" = "${PRIMARY_MONITOR}" ]; then
     export TRAY_POS="right"
   fi
   polybar --log=warning --reload top    2>&1 | tee -a /tmp/polybar_top.log    & disown

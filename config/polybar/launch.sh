4715cccffbeeb7b71f04af23aba46d2698823341
dotfrag <17456867+dotfrag@users.noreply.github.com>
2022-09-20 09:19:03 +0300

    .
    Former-commit-id: 7594a9a20d335ed37df0880f552e01b1a5a46bb5

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -5,7 +5,7 @@
 killall -q -9 polybar
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+while pgrep -u $UID -x polybar >/dev/null; do sleep 0; done
 
 # Variables
 mapfile -t MONITORS < <(polybar --list-monitors | cut -d":" -f1)

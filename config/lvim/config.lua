378c011c2dc4c6e48b80aee9324b92e87db5f2f2
dotfrag <17456867+dotfrag@users.noreply.github.com>
2022-09-21 16:25:13 +0300

    .
    Former-commit-id: 092d35e9a0610666b02300c823dcb7a5fae5dcc6

--- config/lvim/config.lua
+++ config/lvim/config.lua
@@ -134,7 +134,7 @@ local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
   { command = "black", filetypes = { "python" } },
   { command = "isort", filetypes = { "python" } },
-  { command = "shfmt", filetypes = { "sh" } },
+  { command = "shfmt", extra_args = { "-i", "2" }, filetypes = { "sh" } },
   --   {
   --     -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
   --     command = "prettier",

1da3a4d18e28424f92fbc800d93778fbb9dc7638
dotfrag <17456867+dotfrag@users.noreply.github.com>
2022-09-29 13:19:07 +0300

    .

--- config/i3/scripts/brightness.sh
+++ config/i3/scripts/brightness.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if ! command -v brightnessctl &>/dev/null; then
+  echo "brightnessctl could not be found"
+  notify-send "brightnessctl could not be found"
+  exit 1
+fi
+
 case "$1" in
 up)
   sudo brightnessctl set +5%
@@ -10,4 +16,5 @@ down)
 esac
 
 value=$(sudo brightnessctl info | grep -oP "\d+%")
-notify-send --icon=dialog-information "Brightness" "Value: ${value}"
+command -v canberra-gtk-play &>/dev/null && canberra-gtk-play -i audio-volume-change
+command -v dunstify && dunstify --icon="audio-volume-muted" --hints="string:x-dunst-stack-tag:brightness" "Brightness" "Value: ${value}"

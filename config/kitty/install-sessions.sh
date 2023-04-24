17cece81de42ed51a96d8e893d6c8617fefcf4d9
dotfrag <17456867+dotfrag@users.noreply.github.com>
2023-04-24 17:46:52 +0300

    2023-04-24 17:46:52
    Former-commit-id: 668016afffdc671130cf9610527f6d2415159208

--- config/kitty/install-sessions.sh
+++ config/kitty/install-sessions.sh
@@ -5,6 +5,8 @@ SRC=${DIR}/kitty.desktop
 
 for session in *.session; do
   file_name=${session%.session}
+  file_name_out="kitty-${file_name}.desktop"
   sed "/^Name=/s/\$/ ${file_name}/
-  /^Exec=/s/\$/ --session ${session}/" "${SRC}" >"${DIR}/kitty-${file_name}.desktop"
+  /^Exec=/s/\$/ --session ${session}/" "${SRC}" >"${DIR}/${file_name_out}"
+  echo "Installed ${file_name_out}"
 done

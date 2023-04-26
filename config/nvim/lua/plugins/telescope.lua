f72736849e2f0e18ecec257d58783221a93a5a29
dotfrag <17456867+dotfrag@users.noreply.github.com>
2023-04-26 14:51:50 +0300

    2023-04-26 14:51:50
    Former-commit-id: 4c7b490705c5b4daf777ca234c0ce5d334bb9604

---
+++ config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,23 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  opts = {
+    defaults = {
+      mappings = {
+        i = {
+          ["<C-n>"] = function(...)
+            return require("telescope.actions").cycle_history_next(...)
+          end,
+          ["<C-p>"] = function(...)
+            return require("telescope.actions").cycle_history_prev(...)
+          end,
+          ["<C-j>"] = function(...)
+            return require("telescope.actions").move_selection_next(...)
+          end,
+          ["<C-k>"] = function(...)
+            return require("telescope.actions").move_selection_previous(...)
+          end,
+        },
+      },
+    },
+  },
+}

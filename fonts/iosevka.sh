5aa89420e38c89c151d1806f637a9dee7229ca97
dotfrag <17456867+dotfrag@users.noreply.github.com>
2023-06-04 18:50:53 +0300

    2023-06-04 18:50:52

---
+++ fonts/iosevka.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+LATEST_RELEASE=$(curl -sLH 'Accept: application/json' https://github.com/be5invis/Iosevka/releases/latest)
+LATEST_VERSION=$(echo "$LATEST_RELEASE" | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
+
+cd /tmp || exit 1
+wget -nc "https://github.com/be5invis/Iosevka/releases/download/${LATEST_VERSION}/super-ttc-iosevka-ss08-${LATEST_VERSION//v/}.zip"
+unzip "super-ttc-iosevka-ss08-${LATEST_VERSION//v/}.zip"
+
+mkdir -p ~/.local/share/fonts
+mv -vf /tmp/iosevka-ss08.ttc ~/.local/share/fonts/
+fc-cache -rf

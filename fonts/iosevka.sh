caaf529ffb16ccbef84f704c019512509b679ff6
dotfrag <17456867+dotfrag@users.noreply.github.com>
2023-06-04 20:49:29 +0300

    2023-06-04 20:49:29

--- fonts/iosevka.sh
+++ fonts/iosevka.sh
@@ -2,11 +2,9 @@
 
 LATEST_RELEASE=$(curl -sLH 'Accept: application/json' https://github.com/be5invis/Iosevka/releases/latest)
 LATEST_VERSION=$(echo "$LATEST_RELEASE" | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
+FILENAME="super-ttc-iosevka-ss08-${LATEST_VERSION//v/}.zip"
 
-cd /tmp || exit 1
-wget -nc "https://github.com/be5invis/Iosevka/releases/download/${LATEST_VERSION}/super-ttc-iosevka-ss08-${LATEST_VERSION//v/}.zip"
-unzip "super-ttc-iosevka-ss08-${LATEST_VERSION//v/}.zip"
-
-mkdir -p ~/.local/share/fonts
-mv -vf /tmp/iosevka-ss08.ttc ~/.local/share/fonts/
+mkdir -p "${HOME}/.local/share/fonts"
+wget -nc -q --show-progress -O "/tmp/${FILENAME}" "https://github.com/be5invis/Iosevka/releases/download/${LATEST_VERSION}/${FILENAME}"
+unzip -o "/tmp/${FILENAME}" -d "${HOME}/.local/share/fonts/"
 fc-cache -rf

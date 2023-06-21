4d0c131ab4287ccb48b27b12705a464e4268c743
dotfrag <17456867+dotfrag@users.noreply.github.com>
2023-06-21 09:14:45 +0300

    2023-06-21 09:14:45

--- config/install.sh
+++ config/install.sh
@@ -12,5 +12,7 @@ for i in $(git -C "${SCRIPT_DIR}" ls-tree --name-only main | grep -vP "vim|zsh")
   command -v "${i}" >/dev/null && ln -vsf "${SCRIPT_DIR}/${i}" "${HOME}/.config/${i}"
 done
 
+command -v betterlockscreen >/dev/null && ln -vsf "${SCRIPT_DIR}/betterlockscreenrc" "${HOME}/.config/betterlockscreenrc"
+
 ln -vsf "${SCRIPT_DIR}/zsh/.zshrc.local" "${ZDOTDIR:-${HOME}}/.zshrc.local"
 ln -vsf "${SCRIPT_DIR}/chrome-flags.conf" "${HOME}/.config/chrome-flags.conf"

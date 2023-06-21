0500f9a7a94127439e06188fc862d61e820f46c3
dotfrag <17456867+dotfrag@users.noreply.github.com>
2023-06-21 09:19:38 +0300

    2023-06-21 09:19:38

--- config/install.sh
+++ config/install.sh
@@ -8,9 +8,11 @@ done
 
 echo
 
+output=()
 for i in $(git -C "${SCRIPT_DIR}" ls-tree --name-only main | grep -vP "vim|zsh"); do
-  command -v "${i}" >/dev/null && ln -vsf "${SCRIPT_DIR}/${i}" "${HOME}/.config/${i}"
+  output+=("$(command -v "${i}" >/dev/null && ln -vsf "${SCRIPT_DIR}/${i}" "${HOME}/.config/${i}")")
 done
+printf "%s\n" "${output[@]}" | column -t && echo
 
 command -v betterlockscreen >/dev/null && ln -vsf "${SCRIPT_DIR}/betterlockscreenrc" "${HOME}/.config/betterlockscreenrc"
 

1f9d7b2f511960941f2c74e4e5c351e0c2ddf4ea
dotfrag <17456867+dotfrag@users.noreply.github.com>
2023-07-01 15:57:41 +0300

    2023-07-01 15:57:41

---
+++ config/fuzzel/powermenu.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+lock=' Lock'
+suspend=' Sleep'
+logout='󰍃 Logout'
+reboot=' Reboot'
+poweroff=' Poweroff'
+
+chosen=$(echo -e "$lock\n$suspend\n$logout\n$reboot\n$poweroff" | fuzzel --dmenu --lines 5)
+case ${chosen} in
+  "$poweroff")
+    systemctl poweroff
+    ;;
+  "$reboot")
+    systemctl reboot
+    ;;
+  "$lock")
+    swaylock -f
+    ;;
+  "$suspend")
+    systemctl suspend
+    ;;
+  "$logout")
+    swaymsg exit
+    ;;
+esac

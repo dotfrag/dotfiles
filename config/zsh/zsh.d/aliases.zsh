78ba63ce9e16b6632c65552a4dbeff8a051affab
dotfrag <17456867+dotfrag@users.noreply.github.com>
2023-08-07 16:32:55 +0300

    2023-08-07 16:32:55

---
+++ config/zsh/zsh.d/aliases.zsh
@@ -0,0 +1,51 @@
+# ---------------------------------------------------------------------- ALIASES
+alias agi='sudo nala install'
+alias aug='sudo nala update && sudo nala upgrade'
+alias di='sudo dnf install'
+alias dug='sudo dnf check-update && sudo dnf upgrade'
+
+alias chmod='chmod -v'
+alias chown='chown -v'
+alias cp='cp -vi'
+alias ln='ln -vi'
+alias mv='mv -vi'
+alias rm='rm -v'
+
+alias bathelp='bat --plain --language=help'
+alias cat='bat'
+alias colors='bash -c "$(wget -qO- https://git.io/vQgMr)"'
+alias dfs='df -h | sort -n -k 5'
+alias dus='du -sh * 2>/dev/null | sort -h'
+alias exa='exa --long --git --group-directories-first'
+alias files='nautilus --browser $(pwd) &>/dev/null & disown'
+alias glow='glow -p'
+alias insecssh='ssh -o "StrictHostKeyChecking=no" -o "UserKnownHostsFile=/dev/null" -o "PreferredAuthentications=keyboard-interactive"'
+alias killsigs="kill -l | tr ' ' '\n' | nl"
+alias mdcat='mdcat -p'
+alias print-aliases='print -rl -- ${(k)aliases}'
+alias print-functions='print -rl -- ${(k)functions}'
+alias ran='ranger --choosedir=/tmp/.rangerdir; LASTDIR=$(cat /tmp/.rangerdir); cd "$LASTDIR"'
+alias tx='tmux attach-session || start-tmux.sh'
+alias update-fnm='curl -fsSL https://fnm.vercel.app/install | bash -s -- --skip-shell'
+alias update-grml-zshrc='wget -nv -O ${ZDOTDIR:-${HOME}}/.zshrc https://git.grml.org/f/grml-etc-core/etc/zsh/zshrc'
+alias update-kitty='curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin && exit'
+alias update-starship='curl -sS https://starship.rs/install.sh | sh'
+
+# --------------------------------------------------------------- GLOBAL ALIASES
+alias -g C='|wc -l'
+alias -g D='|delta'
+alias -g F='|fzf -m'
+alias -g G='|rg'
+alias -g H='|head'
+alias -g HH='|head -$((LINES-4))'
+alias -g L='|less'
+alias -g LL='|& less -r'
+alias -g N='2>/dev/null'
+alias -g S='|sk'
+alias -g TR="|tr ' ' '\n'"
+alias -g Y='|copy'
+alias -g YI='|fzf | copy'
+
+# ---------------------------------------------------------------- ABBREVIATIONS
+abk[f]='$(fzf)'
+abk[gall]="fd -td '^\.git$' -IHL -x echo {//} | sort | xargs -I{} git -C {} "
---
+++ config/zsh/zsh.d/functions.zsh
@@ -0,0 +1,141 @@
+# -------------------------------------------------------------------- FUNCTIONS
+# change working dir in shell to last dir in lf on exit
+lfd() {
+  tmp="$(mktemp)"
+  lf -last-dir-path="$tmp" "$@"
+  if [ -f "$tmp" ]; then
+    dir="$(cat "$tmp")"
+    rm -f "$tmp"
+    if [ -d "$dir" ]; then
+      if [ "$dir" != "$(pwd)" ]; then
+        cd "$dir"
+      fi
+    fi
+  fi
+}
+
+# copy file and cd to destination directory
+cpcd () {
+  if (( ARGC != 2 )); then
+    printf 'usage: cpcd <file> <destination>\n'
+    return 1
+  fi
+  if [[ ! -e "$1" ]]; then
+    printf "error: file $1 doesn't exist\n"
+    return 1
+  fi
+  cp -vi "$1" "$2"
+  if [[ -d "$2" ]]; then
+    builtin cd "$2"
+  else
+    builtin cd "$(dirname "$2")"
+  fi
+}
+
+# move file and cd to destination directory
+mvcd () {
+  if (( ARGC != 2 )); then
+    printf 'usage: mvcd <file> <destination>\n'
+    return 1
+  fi
+  if [[ ! -e "$1" ]]; then
+    printf "error: file $1 doesn't exist\n"
+    return 1
+  fi
+  mv -vi "$1" "$2"
+  if [[ -d "$2" ]]; then
+    builtin cd "$2"
+  else
+    builtin cd "$(dirname "$2")"
+  fi
+}
+
+# extract archive to directory based on filename
+x() {
+  if (( ARGC != 1 )); then
+    printf 'usage: x <file>\n'
+    return 1
+  fi
+  if [[ ! -e "$1" ]]; then
+    printf "error: file $1 doesn't exist\n"
+    return 1
+  fi
+  case "$1" in
+    *.tar.bz|*.tar.bz2) mkdir "${1%.*.*}" && tar xjvf "$1" -C "${1%.*.*}" ;;
+    *.tar.gz)           mkdir "${1%.*.*}" && tar xzvf "$1" -C "${1%.*.*}" ;;
+    *.tar.xz)           mkdir "${1%.*.*}" && tar xJvf "$1" -C "${1%.*.*}" ;;
+    *.tbz|*.tbz2)       mkdir "${1%.*}"   && tar xjvf "$1" -C "${1%.*}"   ;;
+    *.tgz)              mkdir "${1%.*}"   && tar xzvf "$1" -C "${1%.*}"   ;;
+    *.txz)              mkdir "${1%.*}"   && tar xJvf "$1" -C "${1%.*}"   ;;
+    *.7z) 7zx"$1" -o"${1%.*}" ;;
+    *.rar) unrar x"$1" "${1%.*}" ;;
+    *.zip) unzip "$1" -d "${1%.*}" ;;
+    *) printf 'archive format not supported\n' ;;
+  esac
+}
+
+# copy to clipboard
+copy() {
+  if [[ "$XDG_SESSION_TYPE" = "wayland" ]]; then
+    wl-copy --trim-newline
+  else
+    xclip -r -selection primary -filter | xclip -r -selection clipboard
+  fi
+}
+
+update-dots() {
+  local dots="${XDG_STATE_HOME:-${HOME}/.local/state}/dots"
+  git -C "${HOME}/repos/dotfiles" ls-files | rg -v 'ttf$' | while read line; do realpath "${HOME}/repos/dotfiles/${line}"; done >"${dots}"
+  git -C "${HOME}/repos/dotfiles-private" ls-files | while read line; do realpath "${HOME}/repos/dotfiles-private/${line}"; done >>"${dots}"
+  sort -o "${dots}" -u "${dots}"
+}
+
+# history grep
+hgrep() {
+  fc -Dlim "*$@*" 1
+}
+
+# man pages search
+mans() {
+  man --pager="less -G -p '^\s+$2'" "$1"
+}
+
+# touch executable and edit
+touchx() {
+  if ! [[ -e "$1" ]]; then
+    echo -e "#!/bin/bash\n\n" > "$1"
+  fi
+  chmod +x "$1" >/dev/null
+  $EDITOR + "$1"
+}
+
+# backup/restore file
+bak() {
+  mv -v "$1" "$1.bak"
+}
+ubak() {
+  mv -v "$1" "${1%.bak}"
+}
+
+# ripgrep | less
+rgl() {
+  rg --pretty "$@" | less -RFX
+}
+
+# launch app and exit
+launch() {
+  nohup "$@" >/dev/null 2>/dev/null & disown && exit
+}
+
+# bat hightlight help messages
+help() {
+    "$@" --help 2>&1 | bat --plain --language=help
+}
+
+# switching shell safely and efficiently? http://www.zsh.org/mla/workers/2001/msg02410.html
+bash() {
+   NO_SWITCH="yes" command bash "$@"
+}
+restart () {
+   exec $SHELL $SHELL_ARGS "$@"
+}

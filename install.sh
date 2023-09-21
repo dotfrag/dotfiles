2b8a5d406da8253a06a0cfd4948e2de2d232be79
dotfrag <17456867+dotfrag@users.noreply.github.com>
2023-09-21 11:46:59 +0300

    2023-09-21 11:46:59

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ -f "setup.sh" ]]; then
-  exec bash setup.sh
+  exec bash setup.sh "$@"
 fi
 
 if ! command -v git &>/dev/null; then
@@ -13,6 +13,6 @@ if [[ ! -d "dotfiles" ]]; then
   git clone https://github.com/dotfrag/dotfiles.git
 fi
 
-bash dotfiles/setup.sh
+bash dotfiles/setup.sh "$@"
 
 chsh -s /bin/zsh

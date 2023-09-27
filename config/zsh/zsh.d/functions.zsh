eca13afed2a748b30d97fe4217ed8453e88d8e8f
dotfrag <17456867+dotfrag@users.noreply.github.com>
2023-09-27 15:48:49 +0300

    2023-09-27 15:48:49

--- config/zsh/zsh.d/functions.zsh
+++ config/zsh/zsh.d/functions.zsh
@@ -1,17 +1,7 @@
 # -------------------------------------------------------------------- FUNCTIONS
 # change working dir in shell to last dir in lf on exit
 lfd() {
-  tmp="$(mktemp)"
-  lf -last-dir-path="$tmp" "$@"
-  if [ -f "$tmp" ]; then
-    dir="$(cat "$tmp")"
-    rm -f "$tmp"
-    if [ -d "$dir" ]; then
-      if [ "$dir" != "$(pwd)" ]; then
-        cd "$dir"
-      fi
-    fi
-  fi
+  cd "$(command lf -print-last-dir "$@")"
 }
 
 # copy file and cd to destination directory

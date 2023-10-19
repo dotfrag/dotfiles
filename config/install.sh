ea70addbcb6116536b1a8bba4439d64984f2afd9
dotfrag <17456867+dotfrag@users.noreply.github.com>
2023-10-19 11:31:25 +0300

    2023-10-19 11:31:25

--- config/install.sh
+++ config/install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+CONFIG_DIR=${XDG_CONFIG_HOME:-${HOME}/.config}
+ZSH_DIR=${ZDOTDIR:-${HOME}}
 
 output=()
 declare -A config_dirs=(
   ["betterlockscreen"]="betterlockscreenrc"
+  ["fast-theme"]="fsh"
   ["google-chrome"]="chrome-flags.conf"
   ["google-chrome-stable"]="chrome-flags.conf"
   ["networkmanager_dmenu"]="networkmanager-dmenu"
@@ -13,16 +16,17 @@ declare -A config_dirs=(
 )
 
 for i in $(git -C "${SCRIPT_DIR}" ls-tree --name-only main | grep -vP "install|zsh"); do
-  rm -rf "${HOME}/.config/${i}"
-  output+=("$(command -v "${i}" >/dev/null && ln -vsf "${SCRIPT_DIR}/${i}" "${HOME}/.config/${i}")")
+  rm -rf "${CONFIG_DIR:?}/${i}"
+  output+=("$(command -v "${i}" >/dev/null && ln -vsf "${SCRIPT_DIR}/${i}" "${CONFIG_DIR}/${i}")")
 done
 
 for i in "${!config_dirs[@]}"; do
   if [[ "$i" = zsh ]]; then
-    output+=("$(command -v zsh >/dev/null && ln -vsf "${SCRIPT_DIR}/zsh/${config_dirs[$i]}" "${ZDOTDIR:-${HOME}}/${config_dirs[$i]}")")
+    output+=("$(command -v zsh >/dev/null && ln -vsf "${SCRIPT_DIR}/zsh/${config_dirs[$i]}" "${ZSH_DIR}/${config_dirs[$i]}")")
+    output+=("$(ln -vsf "${SCRIPT_DIR}/${config_dirs['fast-theme']}" "${HOME}/.config/${config_dirs['fast-theme']}")")
     continue
   fi
-  output+=("$(command -v "${i}" >/dev/null && ln -vsf "${SCRIPT_DIR}/${config_dirs[$i]}" "${HOME}/.config/${config_dirs[$i]}")")
+  output+=("$(command -v "${i}" >/dev/null && ln -vsf "${SCRIPT_DIR}/${config_dirs[$i]}" "${CONFIG_DIR}/${config_dirs[$i]}")")
 done
 
 # shellcheck disable=SC2207

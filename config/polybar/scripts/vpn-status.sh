3c4e9b67df1f6297f119a2438295881eeb353a55
dotfrag <17456867+dotfrag@users.noreply.github.com>
2024-01-30 16:16:36 +0200

    2024-01-30 16:16:36

--- config/polybar/scripts/vpn-status.sh
+++ config/polybar/scripts/vpn-status.sh
@@ -3,7 +3,7 @@
 status() {
   if nmcli c show --active | grep -q vpn; then
     ip=$(ip -f inet addr show tun0 | awk '/inet / {print $2}' | cut -d'/' -f1)
-    echo "󰖂 $ip"
+    echo "󰖂 ${ip}"
   else
     wired_intf=$(ip -br l | awk '$1 !~ "lo|tun|vir|wl" {print $1}')
     subnet_mask=$(ip -f inet addr show "${wired_intf}" | awk '/inet / {print $2}' | cut -d'/' -f2)
@@ -19,7 +19,7 @@ monitor() {
   status
 
   nmcli monitor | while read -r event; do
-    if echo "$event" | grep 'tun0: connected' || echo "$event" | grep 'tun0: device removed'; then
+    if echo "${event}" | grep 'tun0: connected' || echo "${event}" | grep 'tun0: device removed'; then
       status
     fi
   done

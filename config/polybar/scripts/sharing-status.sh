9ed048577affa8f6800428d43f01c47e3cbb305b
dotfrag <17456867+dotfrag@users.noreply.github.com>
2024-01-30 16:16:36 +0200

    2024-01-30 16:16:36

---
+++ config/polybar/scripts/sharing-status.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+i3-msg -t subscribe -m '[ "window" ]' | while read -r event; do
+  name=$(echo "${event}" | jq -r '.container.name')
+  if [ "${name}" = 'teams.microsoft.com is sharing your screen.' ]; then
+    change=$(echo "${event}" | jq -r '.change')
+    if [ "${change}" = "new" ]; then
+      echo "%{F#BF616A}󰊻 SHARING%{F-}"
+    elif [ "${change}" = "close" ]; then
+      echo ""
+    fi
+  fi
+done

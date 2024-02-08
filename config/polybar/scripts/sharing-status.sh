edb2eea825a8295df6d331cc580ad488947c3163
dotfrag <17456867+dotfrag@users.noreply.github.com>
2024-02-08 14:19:08 +0200

    2024-02-08 14:19:08

--- config/polybar/scripts/sharing-status.sh
+++ config/polybar/scripts/sharing-status.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+
+sep="$("${SCRIPT_DIR}/get-property.sh" sep)"
+color=$("${SCRIPT_DIR}/get-property.sh" color tertiary)
+
 i3-msg -t subscribe -m '[ "window" ]' | while read -r event; do
   name=$(echo "${event}" | jq -r '.container.name')
   if [[ "${name}" == "teams.microsoft.com is sharing"* ]]; then
     change=$(echo "${event}" | jq -r '.change')
     if [ "${change}" = "new" ]; then
-      echo "%{F#BF616A}󰊻 SHARING%{F-}"
+      echo "%{F#BF616A}󰊻 SHARING%{F-}%{F${color}}${sep}%{F-}"
     elif [ "${change}" = "close" ]; then
       echo
     fi

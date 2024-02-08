e867e8c1f03426584a5d74efa59e4c50e2f5ce96
dotfrag <17456867+dotfrag@users.noreply.github.com>
2024-02-08 19:02:06 +0200

    2024-02-08 19:02:06

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -9,7 +9,6 @@ while pgrep -u ${UID} -x polybar >/dev/null; do sleep 0; done
 
 # Variables
 mapfile -t MONITORS < <(polybar --list-monitors | cut -d":" -f1)
-PRIMARY_MONITOR=$(polybar --list-monitors | grep "primary" | cut -d":" -f1)
 if [ -z "${POLYBAR_INTF_TYPE}" ]; then
   has_ethernet=$(nmcli device | awk '$2=="ethernet" {print $1}' | head -1 | wc -l)
   if [ "${has_ethernet}" -eq 0 ]; then
@@ -21,12 +20,8 @@ fi
 printf "\n-------------------\n" | tee -a /tmp/polybar_top.log /tmp/polybar_bottom.log
 for m in "${MONITORS[@]}"; do
   export MONITOR=${m}
-  if [ "${m}" = "${PRIMARY_MONITOR}" ]; then
-    export TRAY_POS="right"
-  fi
   for bar in top bottom; do
     polybar --log=warning --reload "${bar}" 2>&1 | tee -a "/tmp/polybar_${bar}.log" &
     disown
   done
-  unset TRAY_POS
 done

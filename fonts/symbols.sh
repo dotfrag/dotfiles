d1086a0548c15af3f6e16051b1c4c7707879bd67
dotfrag <17456867+dotfrag@users.noreply.github.com>
2024-05-27 09:53:23 +0300

    2024-05-27 09:53:23

---
+++ fonts/symbols.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [[ -f /etc/arch-release ]]; then
+  sudo pacman -S --needed "ttf-nerd-fonts-symbols"
+  exit
+fi
+
+LATEST_VERSION=$(curl -sLH "Accept: application/json" "https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest" | grep -Po '"tag_name": "\Kv[^"]*')
+FILENAME="NerdFontsSymbolsOnly.zip"
+
+mkdir -p "${HOME}/.local/share/fonts"
+wget -nc -q --show-progress -O "/tmp/${FILENAME}" "https://github.com/ryanoasis/nerd-fonts/releases/download/${LATEST_VERSION}/${FILENAME}"
+unzip -o "/tmp/${FILENAME}" -d "${HOME}/.local/share/fonts/"
+fc-cache -rf

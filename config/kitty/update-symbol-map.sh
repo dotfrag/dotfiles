f836a351bbfb5f9b931b8164b7b33d73c199f6d6
dotfrag <17456867+dotfrag@users.noreply.github.com>
2024-05-30 16:18:00 +0300

    2024-05-30 16:18:00

--- config/kitty/update-symbol-map.sh
+++ config/kitty/update-symbol-map.sh
@@ -5,4 +5,4 @@ charset=$(fc-match --format='%{charset}\n' 'Symbols Nerd Font Mono' |
             s/([\w\d]{4})/U+$1/g;
             s/ /,/g')
 
-sed -i "s/symbol_map.*/symbol_map ${charset} Symbols Nerd Font Mono/" kitty.conf
+echo "symbol_map ${charset} Symbols Nerd Font Mono" >symbol-map.conf
--- fonts/font-patcher.sh
+++ fonts/font-patcher.sh
@@ -43,14 +43,14 @@ download_and_extract_font() {
 patch_complete() {
   rm -f "${IN_DIR:?}"/* "${OUT_DIR:?}"/*
   cp -t "${IN_DIR}" "${ROOT_DIR}"/*.ttf
-  ${DOCKER} run --rm -v "${IN_DIR}:/in" -v "${OUT_DIR}:/out" nerdfonts/patcher --complete
+  ${DOCKER} run --rm -v "${IN_DIR}:/in" -v "${OUT_DIR}:/out" nerdfonts/patcher --complete --adjust-line-height
   mv "${OUT_DIR:?}"/*.ttf "${SCRIPT_DIR}/"
 }
 
 patch_complete_variable_width_glyphs() {
   rm -f "${IN_DIR:?}"/* "${OUT_DIR:?}"/*
   cp -t "${IN_DIR}" "${ROOT_DIR}"/IosevkaSS08-{Regular,Medium}.ttf
-  ${DOCKER} run --rm -v "${IN_DIR}:/in" -v "${OUT_DIR}:/out" nerdfonts/patcher --complete --variable-width-glyphs
+  ${DOCKER} run --rm -v "${IN_DIR}:/in" -v "${OUT_DIR}:/out" nerdfonts/patcher --complete --adjust-line-height --variable-width-glyphs
   mv "${OUT_DIR:?}"/*.ttf "${SCRIPT_DIR}/"
 }
 

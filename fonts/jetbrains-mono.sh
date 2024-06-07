f9c18a909ab2534be9826e02b07718f650333b30
dotfrag <17456867+dotfrag@users.noreply.github.com>
2024-06-07 11:09:15 +0300

    2024-06-07 11:09:15

---
+++ fonts/jetbrains-mono.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [[ -f /etc/arch-release ]]; then
+  sudo pacman -S --needed ttf-jetbrains-mono
+  exit
+fi
+
+LATEST_VERSION=$(curl -sLH 'Accept: application/json' 'https://api.github.com/repos/JetBrains/JetBrainsMono/releases/latest' | grep -Po '"tag_name": "\Kv[^"]*')
+FILENAME=JetBrainsMono-${LATEST_VERSION/v/}.zip
+TARGET_DIR="${HOME}/.local/share/fonts/JetBrainsMono"
+
+mkdir -p "${TARGET_DIR}"
+wget -nc -q --show-progress -O "/tmp/${FILENAME}" "https://github.com/JetBrains/JetBrainsMono/releases/download/${LATEST_VERSION}/${FILENAME}"
+unzip -o "/tmp/${FILENAME}" -d "${TARGET_DIR}"
+fc-cache -rf

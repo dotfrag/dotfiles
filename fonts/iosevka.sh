a489492e10e89eb5e1bb6dd3a23a41474ecdbb76
dotfrag <17456867+dotfrag@users.noreply.github.com>
2024-06-07 11:09:15 +0300

    2024-06-07 11:09:15

--- fonts/iosevka.sh
+++ fonts/iosevka.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 if [[ -f /etc/arch-release ]]; then
-  sudo pacman -S --needed "ttc-iosevka-ss08"
+  sudo pacman -S --needed ttc-iosevka-ss08
   exit
 fi
 
 LATEST_VERSION=$(curl -sLH 'Accept: application/json' 'https://api.github.com/repos/be5invis/Iosevka/releases/latest' | grep -Po '"tag_name": "\Kv[^"]*')
 FILENAME=SuperTTC-IosevkaSS08-${LATEST_VERSION/v/}.zip
+TARGET_DIR="${HOME}/.local/share/fonts"
 
-mkdir -p "${HOME}/.local/share/fonts"
+mkdir -p "${TARGET_DIR}"
 wget -nc -q --show-progress -O "/tmp/${FILENAME}" "https://github.com/be5invis/Iosevka/releases/download/${LATEST_VERSION}/${FILENAME}"
-unzip -o "/tmp/${FILENAME}" -d "${HOME}/.local/share/fonts/"
+unzip -o "/tmp/${FILENAME}" -d "${TARGET_DIR}"
 fc-cache -rf

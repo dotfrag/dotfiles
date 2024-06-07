eea192effbbe85cf0519edc33673a26e36ec9c56
dotfrag <17456867+dotfrag@users.noreply.github.com>
2024-06-07 11:39:41 +0300

    2024-06-07 11:39:41

---
+++ fonts/geist.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [[ -f /etc/arch-release ]]; then
+  yay -S --needed otf-geist
+  exit
+fi
+
+LATEST_VERSION=$(curl -sLH 'Accept: application/json' 'https://api.github.com/repos/vercel/geist-font/releases/latest' | grep -Po '"tag_name": "\K[^"]*')
+FILENAMES=("Geist-${LATEST_VERSION}.zip" "GeistMono-${LATEST_VERSION}.zip")
+TARGET_DIR="${HOME}/.local/share/fonts"
+
+mkdir -p "${TARGET_DIR}"
+for file in "${FILENAMES[@]}"; do
+  wget -nc -q --show-progress -O "/tmp/${file}" "https://github.com/vercel/geist-font/releases/download/${LATEST_VERSION}/${file}"
+  unzip -o "/tmp/${file}" -d "${TARGET_DIR}" -x '__MACOSX/*' '*/statics-otf/*' '*statics-woff/*' '*/variable-woff/*'
+done
+fc-cache -rf
---
+++ fonts/inter.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [[ -f /etc/arch-release ]]; then
+  yay -S --needed ttf-inter
+  exit
+fi
+
+LATEST_VERSION=$(curl -sLH 'Accept: application/json' 'https://api.github.com/repos/rsms/inter/releases/latest' | grep -Po '"tag_name": "\Kv[^"]*')
+FILENAME=Inter-${LATEST_VERSION/v/}.zip
+TARGET_DIR="${HOME}/.local/share/fonts/Inter"
+
+mkdir -p "${TARGET_DIR}"
+wget -nc -q --show-progress -O "/tmp/${FILENAME}" "https://github.com/rsms/inter/releases/download/${LATEST_VERSION}/${FILENAME}"
+unzip -o "/tmp/${FILENAME}" -d "${TARGET_DIR}" -x 'extras/*' 'web/*'
+fc-cache -rf

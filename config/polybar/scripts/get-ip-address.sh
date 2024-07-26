9723bb772d22fa518b752cc4a47daa91314b32a0
dotfrag <17456867+dotfrag@users.noreply.github.com>
2024-07-26 10:48:52 +0300

    2024-07-26 10:48:52

--- config/polybar/scripts/get-ip-address.sh
+++ config/polybar/scripts/get-ip-address.sh
@@ -15,6 +15,14 @@ case "$1" in
     ;;
 esac
 
+# shellcheck disable=SC2154
+if [[ $1 == "vpn" ]] && ! ping -c1 "${VPN_CHECK_HOST}" &>/dev/null; then
+  notify-send "VPN is down, restarting..."
+  vpn=$(nmcli conn show --active | grep vpn | awk '{print $1}')
+  nmcli conn down "${vpn}"
+  nmcli conn up "${vpn}"
+fi
+
 interface=$(ip -br l | awk '$1 !~ "'"${filter}"'" {print $1}')
 ip=$(ip -f inet addr show "${interface}" | awk '/inet / {print $2}' | cut -d'/' -f1)
 

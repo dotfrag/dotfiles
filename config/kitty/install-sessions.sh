3d4c0968818f8b33fa4432c40748288d3baf0682
dotfrag <17456867+dotfrag@users.noreply.github.com>
2024-10-10 10:56:14 +0300

    2024-10-10 10:56:14

--- config/kitty/install-sessions.sh
+++ config/kitty/install-sessions.sh
@@ -7,6 +7,6 @@ for session in *.session; do
   file_name=${session%.session}
   file_name_out="kitty-${file_name}.desktop"
   sed "/^Name=/s/\$/ ${file_name}/
-  /^Exec=/s/\$/ --session ${session}/" "${SRC}" >"${DIR}/${file_name_out}"
+  /^Exec=/s/\$/ -o allow_remote_control=yes --session ${session}/" "${SRC}" >"${DIR}/${file_name_out}"
   echo "Installed ${file_name_out}"
 done

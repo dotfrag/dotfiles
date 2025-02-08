a67eb42ad28f16d04c042083095e74e28a0d2c99
dotfrag <17456867+dotfrag@users.noreply.github.com>
2025-02-08 15:13:23 +0200

    2025-02-08 15:13:23

--- packages.sh
+++ packages.sh
@@ -1,5 +1,10 @@
 # shellcheck shell=bash
-# shellcheck disable=SC2034
+
+# Note: For ttf-nerd-fonts-symbols-mono, use the "Symbols Nerd Font Mono" family
+# in your font config.
+#
+# Symlink /usr/share/fontconfig/conf.avail/10-nerd-font-symbols.conf to
+# /etc/fonts/conf.d/, or see `man 5 fonts-conf` for other options.
 
 packages_pacman=(
   7zip
@@ -21,6 +26,7 @@ packages_pacman=(
   git
   git-delta
   git-filter-repo
+  github-cli
   glow
   gnome-keyring
   imagemagick
@@ -121,3 +127,48 @@ packages_apt=(
   tree
   zsh
 )
+
+detect_distro() {
+  if [[ -f /etc/os-release ]]; then
+    . /etc/os-release
+    DISTRO=${NAME}
+  elif command -v lsb_release &>/dev/null; then
+    DISTRO=$(lsb_release -si)
+  elif [[ -f /etc/lsb-release ]]; then
+    # shellcheck disable=SC1091
+    . /etc/lsb-release
+    # shellcheck disable=SC2154
+    DISTRO=${DISTRIB_ID}
+  else
+    echo "Unable to detect distribution."
+    exit 1
+  fi
+}
+
+setup_packages() {
+  if [[ "${DISTRO}" = "Arch Linux" ]]; then
+    sudo pacman -Syu
+    sudo pacman -S --needed "${packages_pacman[@]}"
+    if ! command -v yay &>/dev/null; then
+      git clone --depth 1 https://aur.archlinux.org/yay-bin.git /tmp/yay &&
+        cd /tmp/yay &&
+        makepkg -si &&
+        command rm -rf /tmp/yay
+    fi
+    yay -S --needed "${packages_aur[@]}"
+  elif [[ "${DISTRO}" = "Fedora Linux" ]]; then
+    sudo dnf check-update
+    sudo dnf install -y "${packages_dnf[@]}"
+  elif [[ "${DISTRO}" = "Ubuntu" ]]; then
+    command -v nala &>/dev/null && apt=nala || apt=apt
+    sudo "${apt}" update
+    sudo "${apt}" install -y "${packages_apt[@]}"
+  fi
+}
+
+main() {
+  detect_distro
+  setup_packages
+}
+
+sudo -v && main

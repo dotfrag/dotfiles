93163c06e568146ea063a422f71945a378acf7c7
dotfrag <17456867+dotfrag@users.noreply.github.com>
2025-03-20 15:28:45 +0200

    2025-03-20 15:28:45

--- config/polybar/scripts/inhibit-idle.sh
+++ config/polybar/scripts/inhibit-idle.sh
@@ -2,8 +2,8 @@
 
 if pgrep -f idle.py >/dev/null; then
   pkill -f idle.py
-  polybar-msg action inhibit hook 0
+  polybar-msg action inhibit-idle hook 0
 else
   nohup idle.py -s 30 >/dev/null 2>&1 &
-  polybar-msg action inhibit hook 1
+  polybar-msg action inhibit-idle hook 1
 fi

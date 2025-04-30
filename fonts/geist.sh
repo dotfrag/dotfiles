7c7135b6d235ce569ac7fd56817a628579f1e513
dotfrag <17456867+dotfrag@users.noreply.github.com>
2025-04-30 15:47:05 +0300

    2025-04-30 15:47:05

--- fonts/geist.sh
+++ fonts/geist.sh
@@ -6,12 +6,12 @@ if [[ -f /etc/arch-release ]]; then
 fi
 
 LATEST_VERSION=$(curl -sLH 'Accept: application/json' 'https://api.github.com/repos/vercel/geist-font/releases/latest' | grep -Po '"tag_name": "\K[^"]*')
-FILENAMES=("Geist-${LATEST_VERSION}.zip" "GeistMono-${LATEST_VERSION}.zip")
+FILENAMES=("Geist-v${LATEST_VERSION}.zip" "GeistMono-${LATEST_VERSION}.zip")
 TARGET_DIR="${HOME}/.local/share/fonts"
 
 mkdir -p "${TARGET_DIR}"
 for file in "${FILENAMES[@]}"; do
   wget -nc -q --show-progress -O "/tmp/${file}" "https://github.com/vercel/geist-font/releases/download/${LATEST_VERSION}/${file}"
-  unzip -o "/tmp/${file}" -d "${TARGET_DIR}" -x '__MACOSX/*' '*/statics-otf/*' '*statics-woff/*' '*/variable-woff/*'
+  unzip -o "/tmp/${file}" -d "${TARGET_DIR}" -x '__MACOSX/*' '*/otf/*' '*/webfonts/*'
 done
 [[ $(ps -o stat= -p "${PPID}") == "Ss" ]] && fc-cache -rf

c1aeec7b816c532cd23919d51bbc1b68145a6db7
dotfrag <17456867+dotfrag@users.noreply.github.com>
2025-06-25 11:59:02 +0300

    2025-06-25 11:59:02

--- fonts/cleanup.sh
+++ fonts/cleanup.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
-cd "$(git rev-parse --show-toplevel)" || exit
+if git_root=$(git rev-parse --show-toplevel); then
+  cd "${git_root}" || exit
+else
+  exit 1
+fi
 
-git-filter-repo --invert-paths --path-glob 'fonts/*.ttf'
+remote=$(git remote -v | awk '{print $2}' | sort -u)
+echo "Current remote: ${remote}"
+echo
+
+if ! git-filter-repo --invert-paths --path-glob 'fonts/*.ttf'; then
+  echo
+  echo "Something went wrong."
+  exit 1
+fi
+
+echo
+echo "Looking good. Double check and push:"
+echo "git remote add origin ${remote}"
+echo "git push -fu origin main "

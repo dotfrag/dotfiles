6fa78e7229befcc94bd329d2600fd6953059980b
dotfrag <17456867+dotfrag@users.noreply.github.com>
2025-08-21 16:44:37 +0300

    2025-08-21 16:44:37

--- fonts/geist.sh
+++ fonts/geist.sh
@@ -6,12 +6,10 @@ if [[ -f /etc/arch-release ]]; then
 fi
 
 LATEST_VERSION=$(curl -sLH 'Accept: application/json' 'https://api.github.com/repos/vercel/geist-font/releases/latest' | grep -Po '"tag_name": "\K[^"]*')
-FILENAMES=("Geist-v${LATEST_VERSION}.zip" "GeistMono-${LATEST_VERSION}.zip")
-TARGET_DIR="${HOME}/.local/share/fonts"
+FILENAME=geist-font-${LATEST_VERSION}.zip
+TARGET_DIR="${HOME}/.local/share/fonts/Geist"
 
 mkdir -p "${TARGET_DIR}"
-for file in "${FILENAMES[@]}"; do
-  wget -nc -q --show-progress -O "/tmp/${file}" "https://github.com/vercel/geist-font/releases/download/${LATEST_VERSION}/${file}"
-  unzip -o "/tmp/${file}" -d "${TARGET_DIR}" -x '__MACOSX/*' '*/otf/*' '*/webfonts/*'
-done
+wget -nc -q --show-progress -O "/tmp/${FILENAME}" "https://github.com/vercel/geist-font/releases/download/${LATEST_VERSION}/${FILENAME}"
+unzip -j -o "/tmp/${FILENAME}" -d "${TARGET_DIR}" "${FILENAME%.zip}/fonts/*" # "${FILENAME%.zip}/fonts/GeistMono"
 [[ $(ps -o stat= -p "${PPID}") == "Ss" ]] && fc-cache -rf

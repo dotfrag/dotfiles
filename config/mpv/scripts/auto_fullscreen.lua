bafe8771dbd9a737c02f3f6a53297a1f390ff87a
dotfrag <17456867+dotfrag@users.noreply.github.com>
2025-10-27 10:30:55 +0200

    2025-10-27 10:30:55

--- config/mpv/scripts/auto_fullscreen.lua
+++ config/mpv/scripts/auto_fullscreen.lua
@@ -5,7 +5,7 @@ local paths = {
 mp.register_event("file-loaded", function()
 	local path = mp.get_property("path", "")
 	for _, prefix in ipairs(paths) do
-		if path:find(prefix) then
+		if path:find(prefix) and mp.get_property("video-format") then
 			mp.set_property("fullscreen", "yes")
 			break
 		end

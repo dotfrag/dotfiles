029005fc3cd85aa4bcc959d8e43de63345ed1736
dotfrag <17456867+dotfrag@users.noreply.github.com>
2025-11-19 08:28:08 +0200

    2025-11-19 08:28:08

--- config/zsh/conf.d/completions.zsh
+++ config/zsh/conf.d/completions.zsh
@@ -11,3 +11,11 @@ _upgrayedd() {
     '1:step:(bob bun cargo code docker gh gup nala npm pnpm repos rust rustup snap system yay tldr uv yazi zsh)'
 }
 compdef _upgrayedd upgrayedd
+
+_jqsort() {
+  # _files -g '*.json' # find json files in cwd
+  # _path_files -g '**/*.json' # find json files recursively
+  # _arguments '1:json file:_files -g "*.json"'
+  _arguments '1:json file:_path_files -g "**/*.json"'
+}
+compdef _jqsort jqsort

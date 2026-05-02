b3fc7c3581645a616c061a51d78662b5e646fe2f
dotfrag <17456867+dotfrag@users.noreply.github.com>
2026-05-02 17:35:05 +0300

    2026-05-02 17:35:05

--- config/zsh/conf.d/completions.zsh
+++ config/zsh/conf.d/completions.zsh
@@ -8,7 +8,7 @@ compdef _functions func edfunc
 
 _upgrayedd() {
   _arguments \
-    '1:step:(bob bun cargo code docker gh gup nala npm pnpm repos rust rustup snap system yay tldr uv yazi zsh)'
+    '1:step:(bob bun cargo code docker gh gup nala npm pnpm repos rust rustup snap system tldr uv vp yay yazi zsh)'
 }
 compdef _upgrayedd upgrayedd
 
@@ -16,12 +16,13 @@ _jqsort() {
   # _files -g '*.json' # find json files in cwd
   # _path_files -g '**/*.json' # find json files recursively
   # _arguments '1:json file:_files -g "*.json"'
-  _arguments '1:json file:_path_files -g "**/*.json"'
+  _arguments \
+    '1:json file:_path_files -g "**/*.json"'
 }
 compdef _jqsort jqsort
 
 _fkill() {
-    _arguments \
-        '1:signal:_signals'
+  _arguments \
+    '1:signal:_signals'
 }
 compdef _fkill fkill

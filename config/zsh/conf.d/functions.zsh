3488a24fff9818b6e19c2b855e5e0d5c0bcdb82f
dotfrag <17456867+dotfrag@users.noreply.github.com>
2026-06-02 08:26:30 +0300

    2026-06-02 08:26:30

--- config/zsh/conf.d/functions.zsh
+++ config/zsh/conf.d/functions.zsh
@@ -163,11 +163,10 @@ wlsm() {
 # ----------------------------------------------------------------- FILE MANAGER
 # yazi
 f() {
-  local tmp=$(mktemp -t "yazi-cwd.XXXXXX")
-  yazi "$@" --cwd-file="$tmp"
-  if cwd=$(cat -- "$tmp") && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
-    cd -- "$cwd"
-  fi
+  local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+  command yazi "$@" --cwd-file="$tmp"
+  IFS= read -r -d '' cwd < "$tmp"
+  [ "$cwd" != "$PWD" ] && [ -d "$cwd" ] && builtin cd -- "$cwd"
   command rm -f -- "$tmp"
 }
 

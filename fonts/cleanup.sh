bc07439dcc7fd6a24e1a11e3c06c26fad9f96fc5
dotfrag <17456867+dotfrag@users.noreply.github.com>
2026-07-04 21:58:55 +0300

    2026-07-04 21:58:55

--- fonts/cleanup.sh
+++ fonts/cleanup.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
-if ! git_root=$(git rev-parse --show-toplevel); then
-  exit 1
-fi
-
-cd "${git_root}" || exit
+cd "$(git rev-parse --show-toplevel)" || exit
 
 remote=$(git remote get-url origin)
 echo "Current remote: ${remote}"
 echo
+
+tmp_dir=/tmp/fonts-cleanup
+
+mkdir -p "${tmp_dir}"
+git ls-files fonts/*.ttf | xargs cp -t "${tmp_dir}"
 
 if ! git-filter-repo --invert-paths --path-glob 'fonts/*.ttf'; then
   echo
   echo "Something went wrong."
   exit 1
 fi
+
+cp "${tmp_dir}"/* ./fonts/
 
 echo
 echo "Looking good. Double check and push:"

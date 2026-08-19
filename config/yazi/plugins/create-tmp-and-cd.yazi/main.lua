d7cb7bb7e5ff6c7e11e5c5b2e5446cc41614ca0e
dotfrag <17456867+dotfrag@users.noreply.github.com>
2026-08-19 11:52:18 +0300

    2026-08-19 11:52:18

--- config/yazi/plugins/create-tmp-and-cd.yazi/main.lua
+++ config/yazi/plugins/create-tmp-and-cd.yazi/main.lua
@@ -1,22 +1,22 @@
---- @sync entry
-
 local function entry()
-	-- can't figure out how to use yazi's Command() 🤷
-	local handle = io.popen("mktemp -d")
-	if not handle then
+	local output, err = Command("mktemp"):arg("-d"):output()
+
+	if not output then
 		ya.notify({
 			title = "mktempcd",
-			content = "Failed to run mktemp",
+			content = "Failed to run mktemp: " .. tostring(err),
 			timeout = 3,
 			level = "error",
 		})
 		return
 	end
 
-	local dirname = handle:read("*l")
-	handle:close()
+	-- Trim trailing newline/whitespace from stdout
+	local dirname = output.stdout:gsub("%s+$", "")
 
-	ya.emit("cd", { dirname })
+	if dirname ~= "" then
+		ya.emit("cd", { dirname })
+	end
 end
 
 return { entry = entry }

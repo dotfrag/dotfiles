470e63beacfc8df6a39890c3680746180ad5d8ea
dotfrag <17456867+dotfrag@users.noreply.github.com>
2026-09-05 11:24:49 +0300

    2026-09-05 11:24:49

--- config/sway/scripts/utils.sh
+++ config/sway/scripts/utils.sh
@@ -11,15 +11,23 @@ outputs() {
 }
 
 # Poll tree layout until a window with the given identifier exists
-wait_for() {
+wait_for_window() {
   local what=$1 id=${2:-name}
   until swaymsg -t get_tree | jq -e ".. | objects | select(.${id}? == \"${what}\")" > /dev/null 2>&1; do
     sleep 0.1
   done
 }
 
+# Poll tree layout until a window with the given identifier exists (regex)
+wait_for_window_regex() {
+  local what=$1 id=${2:-name}
+  until swaymsg -t get_tree | jq -e ".. | objects | select(.${id}? | strings | test(\"${what}\"))" > /dev/null 2>&1; do
+    sleep 0.1
+  done
+}
+
 # Poll tree layout until a window with the given title exists
-wait_for_window() {
+wait_for_window_name() {
   local title=$1
   until swaymsg -t get_tree | jq -e ".. | objects | select(.name? == \"${title}\")" > /dev/null 2>&1; do
     sleep 0.1
@@ -27,7 +35,7 @@ wait_for_window() {
 }
 
 # Poll tree layout until a window with the given app_id exists
-wait_for_app_id() {
+wait_for_window_app_id() {
   local app_id=$1
   # select(.app_id? == "$app_id" or .window_properties.class? == "$app_id")
   until swaymsg -t get_tree | jq -e ".. | objects | select(.app_id? == \"${app_id}\")" > /dev/null 2>&1; do

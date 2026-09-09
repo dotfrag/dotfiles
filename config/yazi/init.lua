22f0a8e6a8b25bd09514cdee9ffc35e613abbeb9
dotfrag <17456867+dotfrag@users.noreply.github.com>
2026-09-09 13:55:54 +0300

    2026-09-09 13:55:54

--- config/yazi/init.lua
+++ config/yazi/init.lua
@@ -2,3 +2,10 @@ require("full-border"):setup()
 require("session"):setup({
 	sync_yanked = true,
 })
+
+-- https://github.com/sxyazi/yazi/discussions/4298#discussioncomment-18181077
+km.mgr.rules:insert(1, {
+	on = { "g", "H" },
+	run = string.format("cd -- %s", fs.cwd()),
+	desc = "Go back to where I started",
+})

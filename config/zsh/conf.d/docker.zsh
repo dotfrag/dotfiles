0c3535d2792eb0a849bc1b021f63f25921e11016
dotfrag <17456867+dotfrag@users.noreply.github.com>
2026-09-12 14:47:53 +0300

    2026-09-12 14:47:53

--- config/zsh/conf.d/docker.zsh
+++ config/zsh/conf.d/docker.zsh
@@ -81,7 +81,7 @@ update-container() {
 update-containers() {
   for i in $(docker compose ls | awk '{print $1}' | tail +2 | rg -v caddy); do
     d=$(docker container inspect "${i}" \
-      --format '{{ index .Config.Labels "com.docker.compose.project.working_dir" }}')
+      --format '{{ index .Config.Labels "com.docker.compose.project.working_dir" }}') || continue
     (
       cd "${d}" || exit
       update-container
